c08bee166b04de9f63e13c7cfab91e35cace576f
Christian Vogel <chris.vogel@me.com>
2020-04-25 18:57:09 +0200

    working base app with pug templates

--- app/javascript/components/HelloWorld.tsx
+++ app/javascript/components/HelloWorld.tsx
@@ -7,17 +7,13 @@ interface Props {
 }
 
 export default class HelloWorld extends React.Component<Props> {
+
   render() {
-    console.log('passing username ', this.props.username)
     return template({ username: this.props.username, onClick: this.onClick })
-    // return pug`
-    //   p hello world!!! {this.props.username}
-    // `
   }
 
-  @boundMethod
-  onClick(e: any) {
-    alert('Clicked!')
+  @boundMethod onClick(e: any) {
+    alert('works!')
   }
 
 }
