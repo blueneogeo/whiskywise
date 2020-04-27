2d43cddb95d55001f99c73a4392ad36fc830b5ea
Christian Vogel <chris.vogel@me.com>
2020-04-27 17:25:58 +0200

    implement full integration test of signup and login using headless chrome

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,24 +1,41 @@
 # typed: true
 
 require 'test_helper'
-require 'capybara/rails'
-require 'capybara/minitest'
-require 'capybara/minitest/spec'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   extend T::Sig
 
-  def test_can_log_in_and_out
-    # check homepage
-    visit('/')
-    
-    assert_button('Log out')
+  def test_signup_and_login_flow
+    # check homepage, still logged out
+    visit '/'
+    assert_button 'Log in'
+    assert_button 'Sign up'
+    assert_no_button 'Log out'
 
-    
-    
-    # should not be logged in
-    # expect(page).has_content('Log in')
+    # create user
+    click_button 'Sign up'
+    fill_in 'Username', with: 'John'
+    fill_in 'Password', with: 'testing123'
+    click_button 'Save User'
+
+    # we should be back at the homepage, logged in
+    assert_current_path '/'
+    assert_button 'Log out'
+
+    # now log out
+    click_button 'Log out'
+    assert_button 'Log in'
+    assert_button 'Sign up'
+    assert_no_button 'Log out'
+
+    # and try to log in
     click_button 'Log in'
-    # expect(page)
+    fill_in 'Username', with: 'John'
+    fill_in 'Password', with: 'testing123'
+    click_button 'Login'
+
+    # we should be back at the homepage, logged in
+    assert_current_path '/'
+    assert_button 'Log out'
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'capybara/rails'
+require 'capybara/minitest'
+require 'capybara/minitest/spec'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -13,11 +16,7 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 end
 
-require 'capybara/rails'
-require 'capybara/minitest'
-require 'capybara/minitest/spec'
-
-# Make the Capybara DSL available in all integration tests
+# Make the Capybara DSL and Minitest DSL available in all integration tests
 class ActionDispatch::IntegrationTest
   include Capybara::DSL
   include Capybara::Node::Matchers
